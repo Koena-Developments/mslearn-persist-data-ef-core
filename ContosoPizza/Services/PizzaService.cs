7f7ac5687c5fc82d64b12e8bc111c23e252a8b39
Koena-Devs <116135704+Koena-Developments@users.noreply.github.com>
2025-04-27 12:36:20 +0000

    Making progress on my ASP.NET API

--- ContosoPizza/Services/PizzaService.cs
+++ ContosoPizza/Services/PizzaService.cs
@@ -1,32 +1,60 @@
 using ContosoPizza.Models;
+using ContosoPizza.Data;
 
+using Microsoft.EntityFrameworkCore;
 namespace ContosoPizza.Services;
 
 public class PizzaService
 {
-    public PizzaService()
+    private readonly PizzaContext _context; 
+    public PizzaService(PizzaContext context)
     {
-        
+        _context = context;
     }
 
     public IEnumerable<Pizza> GetAll()
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+                .AsNoTracking()
+                .ToList();
     }
 
     public Pizza? GetById(int id)
     {
-        throw new NotImplementedException();
+        return _context.Pizzas
+                .Include(p => p.Toppings)
+                .Include(p => p.Sauce)
+                .AsNoTracking()
+                .SingleOrDefault(p => p.Id == id);
     }
 
     public Pizza? Create(Pizza newPizza)
     {
-        throw new NotImplementedException();
+        _context.Pizzas.Add(newPizza);
+        _context.SaveChanges();
+        return newPizza;
     }
 
     public void AddTopping(int PizzaId, int ToppingId)
     {
-        throw new NotImplementedException();
+        var UpdatePizza = _context.Pizzas.Find(PizzaId);
+        var WithToppings = _context.Toppings.Find(ToppingId);
+
+        if(UpdatePizza is null || WithToppings is null)
+        {
+             throw new InvalidOperationException("Pizza or topping does not exist");
+        }
+        else if(UpdatePizza.Toppings is null)
+        {
+            UpdatePizza.Toppings = new List<Topping>();
+        }
+
+        UpdatePizza.Toppings.Add(WithToppings);
+
+        _context.SaveChanges();
+
+
+
     }
 
     public void UpdateSauce(int PizzaId, int SauceId)
